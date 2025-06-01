018b03b190d6ab1b41f4b53b305deddcbe5448c6
YZCodeLearn <yehanzhang88@gmail.com>
2025-06-01 10:56:34 -0700

    Update Gundam_Poker_Lite.py

--- Gundam_Poker_Lite.py
+++ Gundam_Poker_Lite.py
@@ -128,7 +128,7 @@ if st.button("✅ Submit Round", use_container_width=True):
 if st.session_state.winner:
     st.success(f"🎉 Game over! {st.session_state.winner} has won the game! 🏆")
     st.balloons()
-if st.button("🔁 Reset Game", use_container_width=True):
+    if st.button("🔁 Reset Game", use_container_width=True):
         for key in list(st.session_state.keys()):
             del st.session_state[key]
         st.rerun()
@@ -161,7 +161,7 @@ ax.legend()
 ax.grid(True)
 st.pyplot(fig)
 
-if st.button("🔁 Reset Game", use_container_width=True):
+if st.button("🔁 Reset Game", use_container_width=True,key='Reset_After_Over'):
         for key in list(st.session_state.keys()):
             del st.session_state[key]
         st.rerun()
